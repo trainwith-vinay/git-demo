3a176c3458adaace67902eef031c22e35eea5fd0
trainwith-vinay <89221104+trainwith-vinay@users.noreply.github.com>
2021-08-25 16:42:35 +0530

    Update Hello.java
    sprint 22

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World - this is from SPRINT 1 ");
+      System.out.println("Hello, World - this is from SPRINT 222222 ");
    }
 }
